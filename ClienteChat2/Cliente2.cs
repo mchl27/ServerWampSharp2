7a13642bd174447f9221a4bffafc4f7cad00ca12
Michael <Michael@MICHAEL>
2023-06-01 22:28:35 -0500

    Agregar archivos de proyecto.

---
+++ ClienteChat2/Cliente2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+
+namespace ClienteChat2
+{
+    internal class Cliente2
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando Cliente #2");
+
+            // Creacion del canal WAMP
+            DefaultWampChannelFactory factory =
+                new DefaultWampChannelFactory();
+
+
+            // Direccion del servidor WAMP
+            const string serverAddress = "ws://127.0.0.1:8080/ws";
+            // Reino del servidor WAMP
+            const string realmName = "realm1";
+            // Nombre del tema de chat
+            const string topic = "com.myapp.chat";
+
+            // Creacion del canal y conexion al servidor WAMP
+            IWampChannel channel =
+                factory.CreateJsonChannel(serverAddress, realmName);
+
+            // Abro la conexion con el servidor
+            await channel.Open().ConfigureAwait(false);
+
+            // Obtengo la interfaz del reino previamente creado
+            IWampRealmProxy realmProxy = channel.RealmProxy;
+
+            // Ingreso de datos del cliente
+            Console.Write("ingrese un usuario: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Bienvenido {username}");
+
+            // Publicar mensaje en el tema chat creado para notificar la entrada del usuario
+            realmProxy.Services.GetSubject<string>(topic)
+                                         .OnNext($"Ha ingresado: {username}");
+
+            // Suscribirse al tema de chat para recibir mensajes
+            IDisposable subscription = null;
+            subscription =
+                realmProxy.Services.GetSubject<string>(topic)
+                          .Subscribe(message =>
+                          {
+                              string[] parts = message.Split(':');
+                              string sender = parts[0];
+                              string content = parts[1];
+
+                              if (sender != username)
+                              {
+                                  Console.WriteLine($"{sender}: {content}");
+                              }
+                          });
+
+            Console.WriteLine("Escribe un mensaje o 'exit' para salir.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    // Publicar mensaje en el tema chat creado para notificar la desconexion del usuario
+                    realmProxy.Services.GetSubject<string>(topic)
+                                         .OnNext($"El usuario: {username} se ha desconectado");
+                    break;
+                }
+
+                string chatMessage = $"{username}:{input}";
+
+                // Publicar mensaje en el tema chat
+                realmProxy.Services.GetSubject<string>(topic)
+                                         .OnNext(chatMessage);
+            }
+
+            // Cerrar el Canal
+            subscription.Dispose();
+            channel.Close();
+        }
+    }
+}
---
+++ ServerWampSharp2/ServerChat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+
+namespace ServerWampSharp2
+{
+    internal class ServerChat
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando servidor espere un momento...");
+
+            // Direccion del servidor WAMP
+            const string serverAddress = "ws://127.0.0.1:8080/ws";
+            // Nombre del reino en el servidor WAMP
+            const string realmName = "realm1";
+            // Nombre del tema del chat
+            const string topic = "com.myapp.chat";
+
+            // Creacion del host WAMP
+            DefaultWampHost host = new DefaultWampHost(serverAddress);
+
+            // Se abre el host para aceptar conexiones
+            host.Open();
+
+            // Obtengo el reino en el host
+            IWampHostedRealm realm = host.RealmContainer.GetRealmByName(realmName);
+
+            // Suscribirse al tema de chat para recibir mensajes
+            IDisposable subscription = null;
+            subscription =
+                realm.Services.GetSubject<string>(topic)
+                     .Subscribe(message =>
+                     {
+                         // Muestra mensaje de la fecha y hora actual junto con el mensaje enviado por el cliente
+                         Console.WriteLine($"{DateTime.Now}: " + message);
+                         // Reenviar mensaje a todos los clientes suscritos al tema
+                         realm.Services.GetSubject<string>(topic).OnNext(message);
+                     });
+
+            Console.WriteLine("Servidor Iniciado en la direccion: " + serverAddress);
+            Console.ReadLine();
+
+            // Cerrar el host
+            subscription.Dispose();
+            host.Dispose();
+        }
+    }
+}
